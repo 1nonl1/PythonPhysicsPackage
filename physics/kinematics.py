f4614a87452be6f6e49f2b454556c1a6f3501df2
1nonly <tobiasbraymen@gmail.com>
2025-11-18 12:22:11 -0600

    Implement final velocity calculations in kinematics.py

--- physics/kinematics.py
+++ physics/kinematics.py
@@ -1,4 +1,30 @@
 #Vf = Vi + at
-#Vf^2 = Vi^2 + 2as
+#Vf = sqrt(Vi^2 + 2as)
 #s= Vit + ((at^2)/2)
 #Vaverage = s/t
+
+def VelAT(a, t, Vi):
+  """
+  Calculates the final velocity if you know the acceleration, time, and initial velocity
+  Args:
+    a (float): Acceleration in m/s^2
+    t (float): Time in seconds
+    Vi (float): Initial velocity in m/s
+  Returns:
+    float: Final velocity of the object
+  """
+  finalVelocity = Vi + (a*t)
+  return finalVelocity
+
+def VelAS(a, s, Vi): 
+  """
+  Calculates the final velocity if you know the acceleration, initial velocity, and displacement
+  Args:
+    a (float): Acceleration in m/s^2
+    s (float): Displacement of object in m
+    Vi (float): Initial velocity in m/s
+  Returns:
+    float: Final velocity of the object
+  """
+  finalVelocity = (Vi + (2 * (a * s))) ** 0.5
+  return finalVelocity
