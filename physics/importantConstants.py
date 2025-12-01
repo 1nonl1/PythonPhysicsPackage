4567a5de405c1dcdeb715fe29e1cfccc0378ea97
1nonly <tobiasbraymen@gmail.com>
2025-12-01 17:55:52 -0600

    Updated some constants and added Boltzmann Constant

--- physics/importantConstants.py
+++ physics/importantConstants.py
@@ -1,7 +1,7 @@
 class Constant():
     c = 299792458  # speed of light in m/s
-    G = 6.674e-11 # Newtons Constant m^3 / kg * s^2
-    h = 6.626e-34 # Planck's constant m^2 kg / s
+    G = 6.6743e-11 # Newtons Constant m^3 / kg * s^2
+    h = 6.62607015e-34 # Planck's constant m^2 kg / s
     PI = 3.14159265359
     EARTH_GRAVITY = 9.807 #m/s^2
-    
+    Kb = 1.380649e-23 #Boltzmann Constant m^2 kg s^-2 K^-1
